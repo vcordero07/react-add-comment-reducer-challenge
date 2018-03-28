e84941fdf83deb487e80b1a1afb7b58981a90c09
vcordero07 <vcordero07@gmail.com>
2018-03-27 22:42:25 -0400

    wip

--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,25 @@
-import {ADD_COMMENT} from './actions';
+import { ADD_COMMENT } from "./actions";
 
 const initialState = {
-    comments: []
+  comments: []
 };
 
-export default undefined; // Change me!
+function reducer(state = initialState, action) {
+  if (action.type === ADD_COMMENT) {
+    console.log("test");
+    return {
+      ...state,
+      comments: [
+        ...state.comments,
+        {
+          author: action.author,
+          comment: action.comment
+        }
+      ]
+    };
+  } else {
+    return state;
+  }
+}
+
+export default reducer;
